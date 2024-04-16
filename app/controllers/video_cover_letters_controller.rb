23c2d27871255fca20a366ae84c527399e356c84
Ally Han <allyhan@Allys-MBP-2.fios-router.home>
2024-04-16 17:46:35 -0400

    added update video, skipping the CD in CRUD.

--- app/controllers/video_cover_letters_controller.rb
+++ app/controllers/video_cover_letters_controller.rb
@@ -6,4 +6,22 @@ class VideoCoverLettersController < ApplicationController
   def show
     @video = VideoCoverLetter.find(params[:id])
   end
+
+  def edit
+    @video_cover_letter = VideoCoverLetter.find(params[:id])
+  end
+
+  def update
+    @video_cover_letter = VideoCoverLetter.find(params[:id])
+    if @video_cover_letter.update(video_params)
+      redirect_to @video_cover_letter
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def video_params
+      params.require(:video_cover_letter).permit(:title, :description, :url)
+    end
 end
